bde29d83d0948e2930c05daa1a8baa8bb1067fc7
cysabi <47790183+cysabi@users.noreply.github.com>
2024-03-23 17:55:15 -0400

    dialog system woo

---
+++ DialogSystem/dialog_player.gd
@@ -0,0 +1,45 @@
+extends CanvasLayer
+
+@export_file("*.json") var scene_text_file
+
+var scene_text = {}
+var selected_text = []
+var in_progress = false
+
+@onready var background = $Background
+@onready var text_label = $TextLabel
+
+func _ready():
+	background.visible = false
+	scene_text = load_scene_text()
+	DialogBus.display_dialog.connect(on_display_dialog)
+
+func load_scene_text():
+	if FileAccess.file_exists(scene_text_file):
+		var file = FileAccess.open(scene_text_file, FileAccess.READ)
+		return JSON.parse_string(file.get_as_text())
+
+func on_display_dialog(text_key):
+	if in_progress:
+		next_line()
+	else:
+		get_tree().paused = true
+		background.visible = true
+		in_progress = true
+		selected_text = scene_text[text_key].duplicate()
+		show_text()
+
+func show_text():
+	text_label.text = selected_text.pop_front()
+
+func next_line():
+	if selected_text.size() > 0:
+		show_text()
+	else:
+		finish()
+
+func finish():
+	text_label.text = ""
+	background.visible = false
+	in_progress = false
+	get_tree().paused = false
