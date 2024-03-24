959593e8db472d3f306bb21a877e48560e40a5c7
EmalineG <mgayhart0@ivc.edu>
2024-03-24 15:49:56 -0700

    added global boss variable/function and simplied globals

--- PlayerKarma.gd
+++ PlayerKarma.gd
@@ -1,37 +1,19 @@
 extends Node
 
-var round = 0
-var stole = 0
-var helped = 0
-var deaths = 0
-var victories = 0
+var player_strength = 0
+var boss_strength = 0
 
-func increase_round():
-	round += 1
-	print("Round is now: ", round)
+func increase_player_strength():
+	player_strength += 1
+	print("Player strength is now: ", player_strength)
 
-func increase_stole():
-	stole += 1
-	print("Stole is now: ", stole)
-
-func increase_helped():
-	helped += 1
-	print("Help is now:", helped)
-
-func increase_deaths():
-	deaths += 1
-	print("Deaths is now:", deaths)
-	
-func increase_victories():
-	victories += 1
-	print("Victories is now:", victories)
+func increase_boss_strength():
+	boss_strength += 1
+	print("Boss strength is now: ", boss_strength)
 
 func reset_globals():
-	round = 0
-	stole = 0
-	helped = 0
-	deaths = 0
-	victories = 0
+	player_strength = 0
+	boss_strength = 0
 	
 func restart_game():
 	reset_globals()
