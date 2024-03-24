40c996dace1f7488e47e242631e5b74c0d75bc15
cysabi <47790183+LeptoFlare@users.noreply.github.com>
2024-03-23 22:35:13 -0400

    player movement+

--- Characters/player_character.gd
+++ Characters/player_character.gd
@@ -2,14 +2,30 @@ extends CharacterBody2D
 
 @export var move_speed : float = 100
 
-func _physics_process(_delta):
-	# Get input direction
-	var input_direction = Vector2(
+const MAX_SPEED = 300
+const ACCEL = 1500
+const FRICTION = 1200
+var input
+
+func _physics_process(delta):
+	input = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
-	
-	# Update velocity
-	velocity = input_direction * move_speed
-	# 
+	).normalized()
+
+	if input == Vector2.ZERO:
+		apply_friction(FRICTION * delta)
+	else:
+		apply_movement(input * ACCEL * delta)
+
 	move_and_slide()
+
+func apply_friction(amount):
+	if velocity.length() > amount:
+		velocity -= velocity.normalized() * amount
+	else:
+		velocity = Vector2.ZERO
+
+func apply_movement(movement):
+	velocity += movement
+	velocity = velocity.limit_length(MAX_SPEED)
