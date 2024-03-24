dcae65ff496332aa6201e85c0aaf025d9ac64594
cysabi <47790183+LeptoFlare@users.noreply.github.com>
2024-03-24 01:33:23 -0400

    expand dialog stuff idk ajasdklfja;dkf

--- DialogSystem/dialog_player.gd
+++ DialogSystem/dialog_player.gd
@@ -1,45 +1,72 @@
 extends CanvasLayer
 
-@export_file("*.json") var scene_text_file
+@export_file("*.json") var all_text_file
 
-var scene_text = {}
-var selected_text = []
+var all_text = {}
+var current_block = []
 var in_progress = false
 
 @onready var background = $Background
 @onready var text_label = $TextLabel
+@onready var choices_dialog = $ChoicesDialog
+@onready var choices_list = $ChoicesDialog/MarginContainer/Choices
+@onready var choices_prefab = $ChoicesDialog/MarginContainer/Choices/Button
+
+signal SELECTED(index)
 
 func _ready():
-	background.visible = false
-	scene_text = load_scene_text()
+	visible = false
+	choices_dialog.visible = false
+	if FileAccess.file_exists(all_text_file):
+		var file = FileAccess.open(all_text_file, FileAccess.READ)
+		all_text = JSON.parse_string(file.get_as_text())
 	DialogBus.display_dialog.connect(on_display_dialog)
 
-func load_scene_text():
-	if FileAccess.file_exists(scene_text_file):
-		var file = FileAccess.open(scene_text_file, FileAccess.READ)
-		return JSON.parse_string(file.get_as_text())
-
 func on_display_dialog(text_key):
-	if in_progress:
-		next_line()
-	else:
-		get_tree().paused = true
-		background.visible = true
-		in_progress = true
-		selected_text = scene_text[text_key].duplicate()
-		show_text()
+	set_current_block(all_text[text_key])
+	show_text()
+
+func set_current_block(block):
+	current_block = block.duplicate()
+	if typeof(current_block) != TYPE_ARRAY:  # normalize to array
+		current_block = [current_block]
 
 func show_text():
-	text_label.text = selected_text.pop_front()
+	if visible != true:
+		get_tree().paused = true
+		visible = true
+	if current_block.size() == 0:
+		visible = false
+		text_label.text = ""
+		get_tree().paused = false
+		return
 
-func next_line():
-	if selected_text.size() > 0:
-		show_text()
-	else:
-		finish()
+	var current_line = current_block.pop_front()
 
-func finish():
-	text_label.text = ""
-	background.visible = false
-	in_progress = false
-	get_tree().paused = false
+	if typeof(current_line) == TYPE_STRING:
+		show_textline(current_line)
+
+	elif typeof(current_line) == TYPE_DICTIONARY:
+		show_choices(current_line.keys(), current_line.values())
+
+func show_textline(current_line):
+	text_label.text = current_line
+
+func show_choices(choices: Array[String], responses: Array):
+	while choices_list.get_child_count() > 1:
+		var button = choices_list.get_child(choices_list.get_child_count() - 1)
+		choices_list.remove_child(button)
+		button.queue_free()
+	
+	for i in range(choices.size()):
+		if (i == 0):
+			choices_list.get_child(0).text = choices[i]
+		else:
+			choices_list.add_child(choices_prefab.duplicate())
+			choices_list.get_child(i).text = choices[i]
+			choices_list.get_child(i).pressed.connect(show_choices_callback.bind(responses[i]))
+	choices_dialog.visible = true
+func show_choices_callback(response):
+	choices_dialog.visible = false
+	set_current_block(response)
+	show_text()
