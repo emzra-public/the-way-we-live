ba677aef6ed54ada49a18f2cfaf5912244091209
cysabi <47790183+cysabi@users.noreply.github.com>
2024-03-23 22:08:55 -0400

    choices stuff wip

---
+++ DialogSystem/ChoicesDialog.gd
@@ -0,0 +1,33 @@
+extends PanelContainer
+
+signal SELECTED(index)
+
+@onready var choices_list = $"MarginContainer/Choices"
+@onready var choices_prefab = $"MarginContainer/Choices/Button"
+
+var choices:Array[String]:
+	set(value):
+		choices = value
+		initButtons()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	choices_list.get_child(0).pressed.connect(onChoice.bind(0))
+
+func initButtons():
+	while choices_list.get_child_count() > 1:
+		var button = choices_list.get_child(choices_list.get_child_count() - 1)
+		choices_list.remove_child(button)
+		button.queue_free()
+	
+	for i in range(choices.size()):
+		if (i == 0):
+			choices_list.get_child(0).text = choices[i]
+		else:
+			choices_list.add_child(choices_prefab.duplicate())
+			choices_list.get_child(i).text = choices[i]
+			choices_list.get_child(i).pressed.connect(onChoice.bind(i))
+
+func onChoice(choice_index):
+	visible = false
+	SELECTED.emit(choice_index)
---
+++ PlayerKarma.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var round = 0
+
+var stole = 0
+var helped = 0
+
+var deaths = 0
+var victories = 0
