ae9484c6c5d4e6cfca78064321cda7a3b1e87b95
EmalineG <mgayhart0@ivc.edu>
2024-03-23 19:39:41 -0700

    global functions to increase stats, example implementation in onChoice

--- PlayerKarma.gd
+++ PlayerKarma.gd
@@ -1,9 +1,28 @@
 extends Node
 
 var round = 0
-
 var stole = 0
 var helped = 0
-
 var deaths = 0
 var victories = 0
+
+func increase_round():
+	round += 1
+	print("Round is now: ", round)
+
+func increase_stole():
+	stole += 1
+	print("Stole is now: ", stole)
+
+func increase_helped():
+	helped += 1
+	print("Help is now:", helped)
+
+func increase_deaths():
+	deaths += 1
+	print("Deaths is now:", deaths)
+	
+func increase_victories():
+	victories += 1
+	print("Victories is now:", victories)
+
